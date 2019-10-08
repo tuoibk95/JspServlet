5f333b35d432421d12a71f88e7c440d42b850387
tuoibk95 <tuoith95@gmail.com>
2019-10-09 01:50:20 +0700

    Commit added WebSecurity

--- 01_levantuoi_pro_JSPServlet/src/model/WebSecurityModel.java
+++ 01_levantuoi_pro_JSPServlet/src/model/WebSecurityModel.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class WebSecurityModel {
 	public static String encodeHTML(String text) {
 		StringBuilder str = new StringBuilder();
@@ -29,5 +34,42 @@ public class WebSecurityModel {
 			}
 		}
 		return str.toString();
+	}
+	/**
+	 * Description ma hoa 1 doan string theo chuan base64SHA-1
+	 * @param password doan string can encrypt
+	 */
+	public void encrypt(String password) {
+	    try {
+	    	MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	        crypt.reset();
+	        crypt.update(password.getBytes("UTF-8"));
+	    } catch(NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    } catch(UnsupportedEncodingException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	/**
+	 * Description lay ngau nhien 1 loai salt
+	 * @return tra ve 1 doan duoc bien doi
+	 */
+	public byte[] getSalt() {
+		SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[8];
+        random.nextBytes(bytes);
+        return bytes;
+	}
+	
+	/**
+	 * Description replaceWildcart chuyen hoa input sang de tranh loi wildcart
+	 * @param strInput doan string dau vao
+	 * @return tra ve chuoi da duoc chuyen hoa
+	 */
+	public String replaceWildcard(String strInput) {
+		strInput.replace("%", "//%");
+		strInput.replace("_", "//_");
+		return strInput;
 	}
 }
